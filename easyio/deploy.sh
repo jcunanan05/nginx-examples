73187123b97eddfaa6962e3b9296ce0660aeb239
jcunanan05 <cunanan.jonathan1995@gmail.com>
2020-03-07 23:39:53 -0500

    Add easyio

---
+++ easyio/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tar czf easyio.tar.gz main.js package.json yarn.lock public LICENSE
+scp easyio.tar.gz nanogram.io:~
+rm easyio.tar.gz
+
+ssh jonathan@fem_fullstack << 'ENDSSH'
+cd /etc/var/www
+pm2 stop all
+rm -rf easyio
+mkdir easyio
+tar xf easyio.tar.gz -C easyio
+rm easyio.tar.gz
+cd easyio 
+yarn install
+pm2 start all
+ENDSSH
