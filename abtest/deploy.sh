2507deef92aaf9c1877c720136102364a596bad5
jcunanan05 <cunanan.jonathan1995@gmail.com>
2020-03-14 23:51:44 -0400

    add site b for abtest

--- abtest/deploy.sh
+++ abtest/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-tar czf site-a.tar.gz -C abtest/site-a .
-scp site-a.tar.gz jonathan@femfs.jonathancunanan.com:/var/www
-rm site-a.tar.gz
+tar czf site-ab.tar.gz -C abtest/site-ab .
+scp site-ab.tar.gz jonathan@femfs.jonathancunanan.com:/var/www
+rm site-ab.tar.gz
 
 ssh jonathan@fem_fullstack << 'ENDSSH'
 cd /var/www
-rm -rf site-a
-mkdir site-a
-tar xf site-a.tar.gz -C site-a
-rm site-a.tar.gz
+rm -rf site-ab
+mkdir site-ab
+tar xf site-ab.tar.gz -C site-ab
+rm site-ab.tar.gz
 ENDSSH
